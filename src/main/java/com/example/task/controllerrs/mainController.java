b91707d396ff67d7dbfedb7e4ebcf27c8cf16249
VolodymyrBohuslavskiy <936187435BV@gmail.com>
2019-01-12 04:10:28 +0200

    Done

--- src/main/java/com/example/task/controllerrs/mainController.java
+++ src/main/java/com/example/task/controllerrs/mainController.java
@@ -1,9 +1,10 @@
 package com.example.task.controllerrs;
 
-
 import com.example.task.models.User;
 import com.example.task.services.UserService;
 import com.example.task.services.emailService;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.mail.MessagingException;
+import java.util.Date;
 
 @Controller
 public class mainController {
@@ -40,19 +42,35 @@ public class mainController {
     @PostMapping("/add")
     public String add(User user) throws MessagingException {
         user.setPassword(passwordEncoder.encode(user.getPassword()));
-        user.setSecretCode(passwordEncoder.encode(user.getUsername()));
+        String code = user.getUsername();
+        user.setSecretCode(code);
         userService.save(user);
-        System.out.println("http://localhost:8080/authorization/" + user.getSecretCode());
-//        emailService.send(user.getEmail(), "http://localhost:8080/authorization-" + user.getSecretCode());
+
+        String sendcode = Jwts.builder()//Оголошуємо змінну для шифрування
+                .setSubject(code)//Солво що шифрується
+                .signWith(SignatureAlgorithm.HS512, "spring".getBytes())//Обираємо алгоритм шифрування ,слово для дешифрування у байтовий масив
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 30))// термін дії цього jwtoken
+                .compact();// Кінець цього шифруванння
+
+        System.out.println("http://localhost:8080/authorization/" + sendcode);
+        emailService.send(user.getEmail(), "http://localhost:8080/authorization/" + sendcode);
         return "login";
     }
 
     @GetMapping("/authorization/{code}")
     public String authorization(@PathVariable String code) {
-        User user = userService.findBySecretCode(code);
-        user.setEnabled(true);
-        userService.save(user);
-                return "login";
+        try {
+            code = Jwts.parser()//Оголошуємо змінну для дешифрування
+                    .setSigningKey("spring".getBytes())//слово для дешифрування у байтовий масив
+                    .parseClaimsJws(code)//Вставити
+                    .getBody()//Дістаємо боді
+                    .getSubject();
+            User user = userService.findBySecretCode(code);
+            user.setEnabled(true);
+            userService.save(user);
+        } finally {
+            return "login";
+        }
     }
 
 }
